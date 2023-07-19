cb45cfcf1751706271d0d94db209a1ee04f67d15
Nadija30 <126283020+Nadija30@users.noreply.github.com>
2023-07-19 16:26:46 +0300

    commit

--- src/js/01-color-switcher.js
+++ src/js/01-color-switcher.js
@@ -1,1 +1,23 @@
-console.log("Hello")
+const btnStart = document.querySelector('button[data-start]');
+const btnStop = document.querySelector('button[data-stop]');
+const body = document.querySelector('body');
+btnStart.addEventListener('click', onStart);
+btnStop.addEventListener('click', onStop)
+let id = null;
+btnStop.disabled = true;
+function onStart() {
+btnStart.disabled = true;
+btnStop.disabled = false;
+id = setInterval(() => {
+      body.style.backgroundColor = getRandomHexColor();
+  }, 1000);        
+}
+function onStop() {
+btnStart.disabled = false;
+btnStop.disabled = true;
+clearInterval(id);   
+}
+
+function getRandomHexColor() {
+  return `#${Math.floor(Math.random() * 16777215).toString(16).padStart(6, 0)}`;
+}
